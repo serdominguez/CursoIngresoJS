40fc000516884b50447599bd36441cbda8a36d0d
serdominguez <52494370+serdominguez@users.noreply.github.com>
2019-07-03 11:59:35 -0300

    ejercicio 5 con comentarios

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,15 +2,13 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-    //elNombre
-    //laEdad
 
-    var nombre;
-    var edad;
+    var nombre;     //Inicializo variable
+    var edad;       //Inicializo variable
 
-    nombre=document.getElementById('elNombre').value;
-    edad=document.getElementById('laEdad').value;
+    nombre=document.getElementById('elNombre').value;   //traigo valor del cuadro de texto
+    edad=document.getElementById('laEdad').value;       //traigo valor del cuadro de texto
 
-    alert("Usted se llama "+ nombre + " y tiene " + edad + " años")
+    alert("Usted se llama "+ nombre + " y tiene " + edad + " años")     //Imprimo por pantalla en ventana emergente     
 }
 
